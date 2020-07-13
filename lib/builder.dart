8dc5d81d6a801b62578e5114fe84f52941cc802f
shiwenwen <shiwenwendevelop@163.com>
2020-07-13 17:58:02 +0800

    init builder

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,2 +1,10 @@
 /// builder
 /// Created by smindu-sww on 2020/7/13
+
+import 'package:source_gen/source_gen.dart';
+import 'package:build/build.dart';
+
+import 'route_generator.dart';
+
+Builder routeBuilder(BuilderOptions options) => LibraryBuilder(RouteGenerator(),
+    generatedExtension: '.internal_invalid.dart');
