9e1eebaeb47ac1010471cfe4ba37ff2164476ef3
shiwenwen <shiwenwendevelop@163.com>
2020-07-13 17:57:46 +0800

    FRoute 注解定义

--- lib/route.dart
+++ lib/route.dart
@@ -1,4 +1,15 @@
 /// route
 /// Created by smindu-sww on 2020/7/13
+import 'package:fluro/fluro.dart';
 
-class FRoute {}
+class FRoute {
+  final String routePath;
+  final HandlerType handlerType;
+  final HandlerFunc handlerFunc;
+  final TransitionType transitionType;
+  const FRoute(
+      {this.routePath,
+      this.handlerType = HandlerType.function,
+      this.handlerFunc,
+      this.transitionType});
+}
