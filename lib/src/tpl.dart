84451780798fa4f7bc5e00d12043719730d4e150
shiwenwen <shiwenwendevelop@163.com>
2020-07-15 15:29:22 +0800

    完善模板

--- lib/src/tpl.dart
+++ lib/src/tpl.dart
@@ -1,12 +1,11 @@
 /// template
 /// Created by smindu-sww on 2020/7/14
 const String route_tpl = '''
-import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 {{{imports}}}
 
-Router registerFluroRouter() {
-  var router = Router();
+Router registerFluroRouter({Router aRouter}) {
+  final router = aRouter ?? Router();
   {{{routes}}}
   return router;
 }
