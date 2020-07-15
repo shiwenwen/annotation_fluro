7b751c3ec91b9eb2cabe39f14c9c7a8e7cd34ba7
shiwenwen <shiwenwendevelop@163.com>
2020-07-15 15:29:06 +0800

    增加FRootRoute

--- lib/src/route_generator.dart
+++ lib/src/route_generator.dart
@@ -6,11 +6,12 @@ import 'package:build/src/builder/build_step.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 
-import 'annotation.dart';
+import 'base_route.dart';
 import 'collector.dart';
 
-class RouteGenerator extends GeneratorForAnnotation<Annotation> {
+class RouteGenerator extends GeneratorForAnnotation<BaseFRoute> {
   static Collector collector = Collector();
+
   @override
   generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
@@ -19,7 +20,7 @@ class RouteGenerator extends GeneratorForAnnotation<Annotation> {
   }
 }
 
-class RouteWriterGenerator extends GeneratorForAnnotation<Annotation> {
+class RouteWriterGenerator extends GeneratorForAnnotation<FRootRoute> {
   Collector collector() {
     return RouteGenerator.collector;
   }
