c645dde09a37b8b8d2459368c522d2b36caeb2bb
shiwenwen <shiwenwendevelop@163.com>
2020-07-15 15:28:30 +0800

    增加ConstructorElementParse

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,14 +1,25 @@
 /// util
 /// Created by smindu-sww on 2020/7/14
 import 'package:analyzer/src/dart/constant/value.dart';
+import 'package:analyzer/dart/element/element.dart';
 import 'package:source_gen/source_gen.dart';
 
-extension Parse on ConstantReader {
+extension ConstantReaderParse on ConstantReader {
   int get enumRawValue {
     final objectValue = this.objectValue;
     if (objectValue is DartObjectImpl) {
-      return objectValue.fields['function']?.toIntValue();
+      return objectValue.fields.values.first?.toIntValue();
     }
     return null;
+  }
+}
+
+extension ConstructorElementParse on ConstructorElement {
+  String get fullName {
+    String className = enclosingElement.displayName;
+    if (name.isEmpty) {
+      return className;
+    }
+    return '$className.$name';
   }
 }
